320cfd3b88fade5eda875fee7c2f7bf15ecf3883
0xSuku <84302571+0xSuku@users.noreply.github.com>
2024-03-24 16:09:53 -0300

    commit

--- src/helpers/telegram.ts
+++ src/helpers/telegram.ts
@@ -3,7 +3,13 @@ import { Telegraf } from "telegraf";
 const bot = new Telegraf(process.env.ENV_TELEGRAM_TOKEN || "");
 const chatId_URGENT = process.env.ENV_TELEGRAM_CHATID_URGENT || "";
 
-export async function indigoAlert(outputHash: string, asset: string, collateralRatio: number, collateralValue: number, mintedAmount: number) {
+export async function indigoAlert(
+  outputHash: string,
+  asset: string,
+  collateralRatio: number,
+  collateralValue: number,
+  mintedAmount: number
+) {
   await bot.telegram.sendMessage(
     chatId_URGENT,
     `[Indigo]\n\n* CDP: ${outputHash}\n* Asset: ${asset}\n* Ratio: ${collateralRatio.toFixed(
